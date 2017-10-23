84a3eca6c13d02b691c1384a584b1474183009dc
Abdelaziz Khajour <abdelaziz.khajour@gmail.com>
2017-10-23 17:22:59 +0200

    add route table

--- main.tf
+++ main.tf
@@ -22,19 +22,34 @@ resource "aws_internet_gateway" "igw" {
 // ------------------------------------------------------
 // ------------------------------------------------------
 
-resource "aws_route_table" "public_route" {
+
+
+resource "aws_route_table" "public_route_table" {
   vpc_id = "${aws_vpc.terraform.id}"
 
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.igw.id}"
-  }
-
-
   tags {
-    Name = "public_rt"
+    Name = "public_aziz_rt"
   }
 }
+
+resource "aws_route" "public_route" {
+  route_table_id            =  "${aws_route_table.public_route_table.id}"
+  destination_cidr_block    = "0.0.0.0/0"
+  gateway_id = "${aws_internet_gateway.igw.id}"
+}
+
+resource "aws_route_table_association" "rt_association_1" {
+  subnet_id      = "${aws_subnet.public_subnet_1.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+resource "aws_route_table_association" "rt_association_2" {
+  subnet_id      = "${aws_subnet.public_subnet_2.id}"
+  route_table_id = "${aws_route_table.public_route_table.id}"
+}
+
+
+
 // ------------------------------------------------------
 // ------------------------------------------------------
 
@@ -43,7 +58,7 @@ resource "aws_subnet" "public_subnet_1" {
   cidr_block = "172.23.1.0/24"
 
   tags {
-    Name = "public_subnet"
+    Name = "public_subnet_1"
   }
 }
 
@@ -53,6 +68,6 @@ resource "aws_subnet" "public_subnet_2" {
   cidr_block = "172.23.2.0/24"
 
   tags {
-    Name = "private_subnet"
+    Name = "public_subnet_2"
   }
 }
